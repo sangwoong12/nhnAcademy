a21f51553cc13725ea0377b33440bb93b6a10eb3
sangwoong12 <timeattacks2@naver.com>
2023-04-10 11:11:34 +0900

    .DS_Store removed

--- week_7/day_1/springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/messagesender/Main.java
+++ week_7/day_1/springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/messagesender/Main.java
@@ -3,13 +3,14 @@ package com.nhnacademy.edu.springframework.messagesender;
 public class Main {
     public static void main(String[] args) {
         User user = new User("xxx.xxx.com", "010-1234-1234");
-        sendSmsMessage(user,"hello world");
-
+        Main main = new Main();
+        main.sendSmsMessage(user,"hello world");
+        main.sendEmailMessage(user,"hello world");
     }
-    private static void sendSmsMessage(User user, String message){
+    private void sendSmsMessage(User user, String message){
         System.out.println("SMS Message Sent to "+user.getPhoneNumber()+" : "+message);
     }
-    private static void sendEmailMessage(User user, String message){
+    private void sendEmailMessage(User user, String message){
         System.out.println("SMS Message Sent to "+user.getEmail()+" : "+message);
     }
 }
